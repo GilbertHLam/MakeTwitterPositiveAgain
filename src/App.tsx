9a4db3cea25e8a4002165832b1f5b12c7da1d93d
Chi Hang Lam <clmm@amazon.com>
2019-09-18 21:21:51 -0700

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Wave from "./components/wave";
+import "./App.css";
+
+const App: React.FC = () => {
+  return <div className="home">
+    <Wave/>
+  </div>;
+};
+
+export default App;
---
+++ src/components/wave/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Waves from "../../assets/svg/waves/waves";
+import "./styles.css";  
+
+const Wave: React.FC = () => {
+  return (
+    <div>
+        <Waves/>
+    </div>
+  );
+};
+
+export default Wave;
