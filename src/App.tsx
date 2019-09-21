7ebca8cc879561afe783c2350967d70f4bd0a3f2
Gilbert Lam <gilberthlam@cmail.carleton.ca>
2019-09-21 16:24:47 -0700

    Login Page Done (#1)
    * Title and animations
    
    * Sign in button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Wave from "./components/wave";
 import "./App.css";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 const App: React.FC = () => {
-  return <div className="home">
-    <Wave/>
-  </div>;
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Wave from "../../components/wave";
+import LoginButton from "../../components/loginButton";
+import "./styles.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div className="home">
+        <div className="section">
+          <div className="container">
+            <div className="textWrapper">
+              <div className="wordContainer fade-in">
+                <div className="word">
+                  <h1>Make</h1>
+                </div>
+              </div>
+              <div className="wordContainer fade-in">
+                <div className="word">
+                  <h1>Twitter</h1>
+                </div>
+              </div>
+              <div className="wordContainer fade-in">
+                <div className="word">
+                  <h1 className="white">
+                    <span>P</span>
+                    <span>O</span>
+                    <span>S</span>
+                    <span>I</span>
+                    <span>T</span>
+                    <span>I</span>
+                    <span>V</span>
+                    <span>E</span>
+                  </h1>
+                </div>
+              </div>
+              <div className="wordContainer fade-in">
+                <div className="word">
+                  <h1>Again</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+            <div className="button">
+              <LoginButton/>
+            </div>
+        </div>
+      </div>
+
+      <Wave />
+    </>
+  );
+};
+
+export default Home;
